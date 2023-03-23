6bd17685bfd3e9c4744fe4b80977f39120ff20f2
Kishan Shiyal <kishan.shiyal@hotmail.co.uk>
2023-03-23 10:25:22 +0000

    Successfully displayed numbers on minesweeper, and user can now enter coordinates to play the game properly

--- src/main/java/com/softwareinstitute/kishan/shiyal/main/Main.java
+++ src/main/java/com/softwareinstitute/kishan/shiyal/main/Main.java
@@ -27,9 +27,13 @@ public class Main {
         default_board.setBackboard_length(inputLength);
         default_board.setBackboard_width(inputWidth);
 
-        ArrayList<Plate> plate_array = default_board.create_board(default_board.getBackboard_width(), default_board.getBackboard_length());
-        //ArrayList<Plate> plate_array = default_board.check_board(plate_array);
-
+        ArrayList<Plate> initial_plate_array = default_board.create_board(default_board.getBackboard_width(), default_board.getBackboard_length());
+        ArrayList<Plate> plate_array = default_board.check_board(initial_plate_array, default_board.getBackboard_width(), default_board.getBackboard_length() );
+        System.out.println(initial_plate_array);
+        System.out.println(plate_array);
+        for (Plate plate : plate_array) {
+            System.out.println(plate.getState());
+        }
         do {
             System.out.println("\nPlease choose an option: " +
                     "\n1 - Display Test Board " +
@@ -38,13 +42,40 @@ public class Main {
                     "\nChoose your option (1-3):");
             userInput = myObj.nextInt();
             if (userInput == 1){
-                display_backboard(default_board, plate_array);
+                play_game(default_board, plate_array);
             } else if (userInput == 2) {
                 continue;
             } else if (userInput == 3) {
                 breakingLoop = true;
             }
         } while (breakingLoop == false);
+    }
+
+    public static void play_game(Backboard default_board, ArrayList<Plate> plate_array){
+        boolean game_finished = false;
+        Scanner myObj = new Scanner(System.in);
+        do {
+            int enter_x;
+            int enter_y;
+
+            System.out.println("Enter x coord: ");
+            enter_x = myObj.nextInt();
+            System.out.println("Enter y coord: ");
+            enter_y = myObj.nextInt();
+            for (Plate plate : plate_array) {
+                if (plate.getPosition_x() == enter_x && plate.getPosition_y() == enter_y){
+                    if (plate.getState() == 9) {
+                        game_finished = true;
+                    }
+                    plate.setTouched(true);
+                } else {
+                    continue;
+                }
+            }
+            display_backboard(default_board, plate_array);
+        } while (game_finished == false);
+
+        System.out.println("\nOh no you clicked the mine! GAME OVER");
     }
 
     public static void display_backboard (Backboard default_board, ArrayList<Plate> plate_array){
@@ -58,12 +89,18 @@ public class Main {
                 y += 1;
                 System.out.println("");
             }
-
-            if (plate.getState() == 0){
-                System.out.print("O");
-            } else if (plate.getState() == 9){
-                System.out.print("X");
+            if (plate.isTouched()){
+                if (plate.getState() == 0){
+                    System.out.print("O");
+                } else if (plate.getState() >= 1 && plate.getState() <= 8) {
+                    System.out.print(plate.getState());
+                } else if (plate.getState() == 9){
+                    System.out.print("X");
+                }
+            } else {
+                System.out.print("-");
             }
+
         }
     }
 }
--- src/main/java/com/softwareinstitute/kishan/shiyal/minesweeper/Backboard.java
+++ src/main/java/com/softwareinstitute/kishan/shiyal/minesweeper/Backboard.java
@@ -1,7 +1,6 @@
 package com.softwareinstitute.kishan.shiyal.minesweeper;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Backboard {
 
@@ -48,26 +47,196 @@ public class Backboard {
         return plates;
     }
 
-    public ArrayList<Plate> check_board(int x, int y) {
+    public ArrayList<Plate> check_board(ArrayList<Plate> plate_array, int x, int y) {
         ArrayList<Plate> plates = new ArrayList<>();
-        for (int i = 1; i < x+1; i++){
-            for (int j = 1; j < y+1; j++){
-                Plate new_plate = new Plate();
-                new_plate.setPosition_x(j);
-                new_plate.setPosition_y(i);
-                new_plate.setTouched(false);
-                // 0 if blank, 9 if mine
-                double randomNumber = Math.random();
-                // mine should be 15% of the game
-                if (randomNumber <= 0.15) {
-                    new_plate.setState(9);
+
+        for (Plate plate : plate_array) {
+            int display_number = 0;
+            int plate_x = plate.getPosition_x();
+            int plate_y = plate.getPosition_y();
+            if (plate_x == 1 && plate_y == 1) {
+                //top left corner
+                // check right, bottom right, bottom
+                display_number += check_board_right(plate, plate_array, x);
+                display_number += check_board_bottomright(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
                 } else {
-                    new_plate.setState(0);
+                    plate.setState(display_number);
                 }
-                plates.add(new_plate);
+
+            } else if (plate_x == x && plate_y == 1) {
+                //top right corner
+                // check left, bottom left, bottom
+                display_number += check_board_left(plate, plate_array, x);
+                display_number += check_board_bottomleft(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_x == 1 && plate_y == y) {
+                //bottom left corner
+                // check top, top right, right
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topright(plate, plate_array, x);
+                display_number += check_board_right(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_x == x && plate_y == y) {
+                //bottom right corner
+                // check top, top left, left
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topleft(plate, plate_array, x);
+                display_number += check_board_left(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_y == 1) {
+                //top row
+                display_number += check_board_left(plate, plate_array, x);
+                display_number += check_board_bottomleft(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                display_number += check_board_bottomright(plate, plate_array, x);
+                display_number += check_board_right(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_y == y) {
+                //bottom row
+                display_number += check_board_left(plate, plate_array, x);
+                display_number += check_board_topleft(plate, plate_array, x);
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topright(plate, plate_array, x);
+                display_number += check_board_right(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_x == 1) {
+                //left column
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topright(plate, plate_array, x);
+                display_number += check_board_right(plate, plate_array, x);
+                display_number += check_board_bottomright(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else if (plate_x == x) {
+                //right column
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topleft(plate, plate_array, x);
+                display_number += check_board_left(plate, plate_array, x);
+                display_number += check_board_bottomleft(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
+            } else {
+                //check all surroundings
+                display_number += check_board_top(plate, plate_array, x);
+                display_number += check_board_topleft(plate, plate_array, x);
+                display_number += check_board_left(plate, plate_array, x);
+                display_number += check_board_bottomleft(plate, plate_array, x);
+                display_number += check_board_bottom(plate, plate_array, x);
+                display_number += check_board_bottomright(plate, plate_array, x);
+                display_number += check_board_right(plate, plate_array, x);
+                display_number += check_board_topright(plate, plate_array, x);
+                if (plate.getState() == 9) {
+                    System.out.print("");
+                } else {
+                    plate.setState(display_number);
+                }
             }
         }
+        plates = plate_array;
         return plates;
+    }
+
+    public int check_board_topleft(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate topleft = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()-x-2));
+        if (topleft.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_top(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate top = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()-x-1));
+        if (top.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public int check_board_topright(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate topright = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()-x));
+        if (topright.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_left(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate left = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()-2));
+        if (left.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_right(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate right = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()));
+        if (right.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_bottomleft(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate bottomleft = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()+x-2));
+        if (bottomleft.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_bottom(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate bottom = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()+x-1));
+        if (bottom.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int check_board_bottomright(Plate plate, ArrayList<Plate> plate_array, int x) {
+        Plate bottomright = plate_array.get(((x*(plate.getPosition_y()-1))+plate.getPosition_x()+x));
+        if (bottomright.getState() == 9) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public int getBackboard_length() {
